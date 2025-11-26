852b900673da48006cda195ffd1093f4aa8c9309
andkleven <amkleven@gmail.com>
2025-11-26 12:37:37 +0100

    feat: update generateEmail function to fetch bank transactions with date range

--- genrate-email.ts
+++ genrate-email.ts
@@ -2,8 +2,11 @@ import { fetchCatacloudData } from "./catacloud";
 
 export async function generateEmail(token: string): Promise<string> {
   // fetch bank transaction from Catacloud API
-  const bankTransactions = await fetchCatacloudData(token);
-  //
+  const bankTransactions = await fetchCatacloudData(
+    token,
+    "2025-11-01T00:00:00.000Z",
+    "2025-11-30T23:59:59.000Z"
+  );
   // Generate email content using the fetched data and ai
   // const email = await generateEmail(bankTransactions)
   // return email
